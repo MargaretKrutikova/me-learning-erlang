d3d256ec1bb92d8a6663c11e2186253000b3046d
Margarita Krutikova <margaretkru@gmail.com>
2020-10-16 23:54:37 +0200

    Add simple bank account process

--- bank-account/src/bank_account.erl
+++ bank-account/src/bank_account.erl
@@ -13,31 +13,65 @@
         {balance = 0 :: number(),
          status = active :: bank_account_status()}).
 
--spec balance(bank_account) -> number().
+deposit_amount(BankAccount, Amount) ->
+    Balance = BankAccount#bank_account.balance,
+    BankAccount#bank_account{balance = Balance + Amount}.
 
-balance(BankAccount) ->
+get_account_balance(BankAccount) ->
     case BankAccount#bank_account.status of
-        active -> BankAccount#bank_account.balance;
-        closed -> erlang:error(account_closed)
+        active -> {ok, BankAccount#bank_account.balance};
+        closed -> {error, account_closed}
+    end.
+
+close_account(BankAccount) ->
+    BankAccount#bank_account{status = closed}.
+
+%---------------------
+
+bank_account_process(BankAccount) ->
+    receive
+        {From, {deposit, Amount}} ->
+            UpdatedBankAccount = deposit_amount(BankAccount,
+                                                Amount),
+            From ! {ok, UpdatedBankAccount},
+            bank_account_process(UpdatedBankAccount);
+        {From, {close}} ->
+            UpdatedBankAccount = close_account(BankAccount),
+            From ! {ok, 0},
+            bank_account_process(UpdatedBankAccount);
+        {From, {get_balance}} ->
+            From ! get_account_balance(BankAccount),
+            bank_account_process(BankAccount);
+        terminate -> ok
+    end.
+
+balance(Pid) ->
+    Pid ! {self(), {get_balance}},
+    receive
+        {ok, Balance} -> Balance;
+        error -> error
     end.
 
 charge(_Pid, _Amount) -> undefined.
 
--spec close(bank_account) -> number().
+-spec close(pid()) -> number().
 
 close(Pid) ->
-    Balance = balance(Pid),
-    _ = Pid#bank_account{status = closed},
-    Balance.
+    Pid ! {self(), {close}},
+    receive
+        {ok, Amount} -> Amount;
+        _unknown -> io:format("unknown message received", [])
+    end,
+    0.
 
--spec create() -> bank_account.
+start_bank_account() ->
+    bank_account_process(#bank_account{}).
 
-create() -> #bank_account{}.
+create() -> spawn(fun start_bank_account/0).
 
--spec deposit(bank_account, number()) -> bank_account.
+-spec deposit(pid(), number()) -> void.
 
 deposit(Pid, Amount) ->
-    CurrentBalance = balance(Pid),
-    Pid#bank_account{balance = CurrentBalance + Amount}.
+    Pid ! {self(), {deposit, Amount}}.
 
 withdraw(_Pid, _Amount) -> undefined.
