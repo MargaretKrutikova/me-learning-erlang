f0a9bf466394bf90a3e469104118f4ca33454492
Margarita Krutikova <margaretkru@gmail.com>
2020-10-21 21:06:46 +0200

    Fix after review comments on exercism

--- bank-account/src/bank_account_simple.erl
+++ bank-account/src/bank_account_simple.erl
@@ -9,63 +9,6 @@
 
 -record(bank_account, {balance = 0 :: number()}).
 
-% ---- bank account operations ----
-
-update_balance(Account, Amount) ->
-    NewBalance = Account#bank_account.balance + Amount,
-    Account#bank_account{balance = NewBalance}.
-
-deposit_amount(BankAccount, Amount) ->
-    {ok, update_balance(BankAccount, Amount), Amount}.
-
-withdraw_amount(Account, Amount) ->
-    Withdraw = min(Account#bank_account.balance, Amount),
-    {ok, update_balance(Account, -Withdraw), Withdraw}.
-
-charge_amount(Account, Amount)
-    when Amount > Account#bank_account.balance ->
-    {ok, Account, 0};
-charge_amount(Account, Amount) ->
-    {ok, update_balance(Account, -Amount), Amount}.
-
-get_balance(Account) ->
-    {ok, Account, Account#bank_account.balance}.
-
-open_account() -> #bank_account{}.
-
-% ---- process ----
-
-handle_message(Message, State) ->
-    case Message of
-        {deposit, Amount} -> deposit_amount(State, Amount);
-        {withdraw, Amount} -> withdraw_amount(State, Amount);
-        {charge, Amount} -> charge_amount(State, Amount);
-        balance -> get_balance(State)
-    end.
-
-bank_account_process(State) ->
-    receive
-        {From, close} ->
-            From ! State#bank_account.balance,
-            ok;
-        {From, Message} ->
-            {_, NextState, Result} = handle_message(Message, State),
-            From ! Result,
-            bank_account_process(NextState)
-    end.
-
-call_process(Pid, Message) ->
-    send_message(Pid, Message, is_process_alive(Pid)).
-
-send_message(_Pid, _Message, false) ->
-    {error, account_closed};
-send_message(Pid, Message, true) ->
-    Pid ! {self(), Message},
-    receive Msg -> Msg end.
-
-start_bank_process() ->
-    bank_account_process(open_account()).
-
 % ---------- API ----------------
 
 balance(Pid) -> call_process(Pid, balance).
@@ -85,3 +28,62 @@ withdraw(Pid, Amount) ->
 charge(_Pid, Amount) when Amount < 0 -> 0;
 charge(Pid, Amount) ->
     call_process(Pid, {charge, Amount}).
+
+% ---- bank account operations ----
+
+update_balance(Account, Amount) ->
+    NewBalance = Account#bank_account.balance + Amount,
+    Account#bank_account{balance = NewBalance}.
+
+deposit_amount(BankAccount, Amount) ->
+    {update_balance(BankAccount, Amount), Amount}.
+
+withdraw_amount(Account, Amount) ->
+    Withdraw = min(Account#bank_account.balance, Amount),
+    {update_balance(Account, -Withdraw), Withdraw}.
+
+charge_amount(Account, Amount)
+    when Amount > Account#bank_account.balance ->
+    {Account, 0};
+charge_amount(Account, Amount) ->
+    {update_balance(Account, -Amount), Amount}.
+
+get_balance(Account) ->
+    {Account, Account#bank_account.balance}.
+
+open_account() -> #bank_account{}.
+
+% ---- process ----
+
+handle_message(Message, State) ->
+    case Message of
+        {deposit, Amount} -> deposit_amount(State, Amount);
+        {withdraw, Amount} -> withdraw_amount(State, Amount);
+        {charge, Amount} -> charge_amount(State, Amount);
+        balance -> get_balance(State)
+    end.
+
+bank_account_process(State) ->
+    receive
+        {From, close} ->
+            From ! State#bank_account.balance,
+            ok;
+        {From, Message} ->
+            {NextState, Result} = handle_message(Message, State),
+            From ! Result,
+            bank_account_process(NextState)
+    end.
+
+call_process(Pid, Message) ->
+    Pid ! {self(), Message},
+    receive
+        Msg -> Msg
+        after 500 ->
+                  case is_process_alive(Pid) of
+                      true -> {error, timeout};
+                      false -> {error, account_closed}
+                  end
+    end.
+
+start_bank_process() ->
+    bank_account_process(open_account()).
